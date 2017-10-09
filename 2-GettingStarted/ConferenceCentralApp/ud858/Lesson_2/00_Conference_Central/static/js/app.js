500f7c1625ea101fbcd39b232a1cdd7932aabdb1
Anil Kumar <panilkumar16@gmail.com>
2017-10-08 18:25:45 -0700

    feat: Add signin using Google 3rd party authentication
    On Google App Engine Application create OAuth Consent Screen, to
    login using Google OAuth Authentication to the application.
    Create Web Client ID to update in settiings.py and app.js.
    Finally updated code to verify user authentication to view
    and update user profile.

--- 2-GettingStarted/ConferenceCentralApp/ud858/Lesson_2/00_Conference_Central/static/js/app.js
+++ 2-GettingStarted/ConferenceCentralApp/ud858/Lesson_2/00_Conference_Central/static/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'web-client-id',
+        CLIENT_ID: '620384337103-rsv09ipvk4tu4fi1qe3q6tfgh1pq9gc5.apps.googleusercontent.com',
         SCOPES: 'email profile',
         signedIn: false
     }
